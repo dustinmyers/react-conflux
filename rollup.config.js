879d42384d917099abca0cb5ad88f98a83738871
Nathan Thomas <nwthomas@me.com>
2019-05-15 12:25:05 -0700

    Include Rollup ReactDOM import and rollup-plugin-node-resolve

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import uglify from 'rollup-plugin-uglify';
 import babel from 'rollup-plugin-babel';
+import resolve from 'rollup-plugin-node-resolve';
 
 export default {
   input: 'src/index.js',
@@ -8,10 +9,12 @@ export default {
     format: 'cjs',
     name: 'react-conflux',
     globals: {
-      react: 'React'
+      react: 'React',
+      'react-dom': 'ReactDOM'
     }
   },
   plugins: [
+    resolve(),
     babel({
       exclude: 'node_modules/**'
     }),
