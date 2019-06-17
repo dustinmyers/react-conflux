b6556db2b4a6ee6f147e7ea20130f7c4d4534a3f
Nathan Thomas <nwthomas@me.com>
2019-06-17 13:20:33 -0700

    Add background and opaque logo for home page splash

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
 import Layout from '../components/Layout/Layout';
+import confluxAtomLogo from '../images/conflux-atom-white.png';
 
 const index = () => {
   return (
     <Layout>
       <div className="home__container">
-        <section>
-          <div />
+        <section className="home__header">
+          <div className="header__conflux-logo">
+            <img src={confluxAtomLogo} alt="Conflux atom logo in white" />
+          </div>
         </section>
       </div>
     </Layout>
