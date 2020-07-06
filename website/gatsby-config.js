07ec10cd9e721cb1b42ad50448ed7a15ca9381d7
Nathan Thomas <nwthomas@me.com>
2020-07-05 22:31:37 -0700

    Add Gatsby docs template and flesh out initial content

---
+++ website/gatsby-config.js
@@ -0,0 +1,51 @@
+module.exports = {
+  siteMetadata: {
+    siteTitle: `React-Conflux`,
+    defaultTitle: `React-Conflux`,
+    siteTitleShort: `gatsby-theme-docs`,
+    siteDescription: `A modularized state management system for React utilizing the Context API and React Hooks.`,
+    siteUrl: `https://github.com/dustinmyers/react-conflux`,
+    siteAuthor: `Nathan Thomas and Dustin Myers`,
+    siteImage: `/banner.png`,
+    siteLanguage: `en`,
+    themeColor: `#06FFA5`,
+    basePath: `/`,
+    footer: `Made with ❤️`
+  },
+  plugins: [
+    {
+      resolve: `@rocketseat/gatsby-theme-docs`,
+      options: {
+        configPath: `src/config`,
+        docsPath: `src/docs`,
+        githubUrl: `https://github.com/rocketseat/gatsby-themes`,
+        baseDir: `examples/gatsby-theme-docs`
+      }
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Rocketseat Gatsby Themes`,
+        short_name: `RS Gatsby Themes`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        display: `standalone`,
+        icon: `static/favicon.png`
+      }
+    },
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // trackingId: ``,
+      }
+    },
+    {
+      resolve: `gatsby-plugin-canonical-urls`,
+      options: {
+        siteUrl: `https://rocketdocs.netlify.com`
+      }
+    },
+    `gatsby-plugin-offline`
+  ]
+};
